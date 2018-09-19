59f112a9846a0701b03b8b0be3b70a8d611dd0db
rBatt <battrd@gmail.com>
2018-09-19 16:30:29 -0400

    fixed bad merge due to DoW mismatch
    i had merged on the wrong DoW convention -- python has monday as 0, sunday as 6; in R, I had sunday as 1 and saturday as 7 (i think?). This screwed up the merge, causing duplicates whenever a ticket was issued (whenver there was an entry in the original data set).
    
    a side effect of this bad merge was that every ticket was preceded by a 0 value ... which is probably why glm() had been throwing those warnings
    
    thus, i updated model AIC and correlations between fitted and observed
    
    assessment of path forward remains the same

--- mvp_model_sketch.R
+++ mvp_model_sketch.R
@@ -26,7 +26,7 @@ dat[,minute:=as.integer(minute)]
 # ==========================
 reg_datetime_seq <- seq(from=dat[,min(datetime_rnd)], to=dat[,max(datetime_rnd)], by="15 min")
 reg_dt <- data.table(CJ(datetime_rnd=reg_datetime_seq, ViolationCounty=dat[,unique(ViolationCounty)], SubDivision=dat[,unique(SubDivision)]))
-reg_dt[,c("dow","hour","month","minute"):=list(dow=wday(datetime_rnd)-1, hour=hour(datetime_rnd), month=month(datetime_rnd), minute=minute(datetime_rnd))]
+reg_dt[,c("dow","hour","month","minute"):=list(dow=as.integer(format.Date(datetime_rnd, format="%u"))-1, hour=hour(datetime_rnd), month=month(datetime_rnd), minute=minute(datetime_rnd))]
 
 on_cols <- c("datetime_rnd", "ViolationCounty", "SubDivision", "dow", 'hour', "month", 'minute')
 dat2 <- merge(dat, reg_dt, by=on_cols, all=TRUE)
@@ -50,8 +50,8 @@ library(tscount)
 nLag <- 2
 tsVec <- egDat[,counts]
 mod1 <- tscount::tsglm(tsVec, model=list(past_obs=nLag, past_mean=1)) # kinda slow, but not super bad.
-# AIC = 49790.03
-# cor(egDat[,counts], fitted(mod1)) # 0.434887
+# AIC = 46446.16
+# cor(egDat[,counts], fitted(mod1)) # 0.4288084
 # doesn't appear to be reporting all of the coefficients ... reports an intercept, beta_2, and alpha_1.
 # the beta are the autogressive, and the alpha are the moving average
 # it could be that it skips beta_1 and considers beta_1 to be fixed at 1 for lag0 ... but that'd be kinda silly
@@ -68,8 +68,8 @@ tsVec_lag <- embed(tsVec0, nLag+1)[,-(nLag+1)]
 dimnames(tsVec_lag) <- list(NULL, paste0('lag',nLag:1))
 mod2_X <- cbind(y=tsVec, tsVec_lag)
 mod2 <- glm(y~lag1+lag2, family=poisson, data=as.data.frame(mod2_X))
-# AIC = 38623
-# cor(tsVec, fitted(mod2)) # 0.1929
+# AIC = 37343
+# cor(tsVec, fitted(mod2)) # 0.2753909
 
 
 nLag <- 12
@@ -79,8 +79,8 @@ tsVec_lag <- embed(tsVec0, nLag+1)[,-(nLag+1)]
 dimnames(tsVec_lag) <- list(NULL, paste0('lag',nLag:1))
 mod3_X <- cbind(y=tsVec, tsVec_lag)
 mod3 <- glm(y~., family=poisson, data=as.data.frame(mod3_X))
-# AIC = 35193
-# cor(tsVec, fitted(mod3)) # 0.3508718
+# AIC = 35461
+# cor(tsVec, fitted(mod3)) # 0.3381322
 
 
 nLag <- 672 # 672 is number of 15-min observations in 1 week
@@ -90,8 +90,8 @@ tsVec_lag <- embed(tsVec0, nLag+1)[,-(nLag+1)]
 dimnames(tsVec_lag) <- list(NULL, paste0('lag',nLag:1))
 mod4_X <- cbind(y=tsVec, tsVec_lag)
 mod4 <- glm(y~., family=poisson, data=as.data.frame(mod4_X)) # pretty slow; took about 5-10 min
-# AIC = 30693
-# cor(tsVec, fitted(mod4)) # 0.7408245
+# AIC = 29166
+# cor(tsVec, fitted(mod4)) # 0.7437571
 
 
 nLag <- 673 # 672 is number of 15-min observations in 1 week
@@ -108,8 +108,8 @@ use_lags <- c(
 use_lags_names <- paste0("lag",use_lags)
 mod5_X <- mod5_X0[, c("y",use_lags_names)]
 mod5 <- glm(y~., family=poisson, data=as.data.frame(mod5_X)) # fast, just a couple seconds
-# AIC = 36694
-# cor(tsVec, fitted(mod5)) # 0.2899756
+# AIC = 34903
+# cor(tsVec, fitted(mod5)) # 0.3492863
 
 
 nLag <- 673 # 672 is number of 15-min observations in 1 week
@@ -129,8 +129,8 @@ time_indices <- tail(egDat[,list(minute, hour, dow, month)],-nLag)
 time_indices <- sapply(time_indices, function(x)as.factor(x))
 mod6_X <- data.frame(mod6_X, time_indices)
 mod6 <- glm(y~., family=poisson, data=mod6_X) # pretty fast, maybe 5 seconds
-# AIC = 29112
-# cor(tsVec, fitted(mod6)) # 0.4520971
+# AIC = 28959
+# cor(tsVec, fitted(mod6)) # 0.4591734
 
 # ===========
 # = Summary =
